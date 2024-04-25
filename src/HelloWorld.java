a3a556e805b89701e42f3e1c7f2330641786ae76
yu <2156728934@qq.com>
2024-04-25 15:05:57 +0800

    本地库第二次提交

---
+++ src/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("余欣荣");
+        System.out.println("22物联网01");
+        System.out.println("2022133269：");
+    }
+}
